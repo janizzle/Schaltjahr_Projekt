9af4fb7592c003147dae8912a4369298d96efae3
Janos Imhof <janos.imhof@yetnet.ch>
2024-02-28 16:56:00 +0100

    Add source

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javax.swing.*;
+
+import static java.lang.StringTemplate.STR;
+
+public class Main {
+    public static void main(String[] args) {
+        int year = Integer.parseInt(JOptionPane.showInputDialog("Gib ein Jahr ein: "));
+
+        if (year % 100 == 0) {
+            System.out.println(STR."\{year} ist kein Schaltjahr, weil: \{year} ist ein Jahrhundert");
+            return;
+        }
+
+        if (year % 4 == 0) {
+            System.out.println(STR."\{year} ist ein Schaltjahr");
+            return;
+        }
+
+        System.out.println(STR."\{year} ist kein Schaltjahr, weil: Nicht durch 4 Teilbar");
+    }
+}
